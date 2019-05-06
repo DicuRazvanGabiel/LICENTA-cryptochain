802064c7ddc7aafba6cfd89652dd94882d6c448d
Dicu Razvan Gabriel <razvan.dicu@itsix.com>
2019-05-06 19:09:46 +0300

    Transaction Pool

---
+++ app/transaction-miner.js
@@ -0,0 +1,20 @@
+class TransactionMiner {
+
+    constructor({ blockchain, transactionPool, wallet, pubsub }){
+        this.blockchain = blockchain;
+    }
+
+    mineTransactions() {
+        //get the transaction pool's valid transactions
+
+        // generate the miner's reward
+
+        // add a block consisting of these transaction to the blockchain
+
+        // brodcast the updated blockchain
+
+        // clear the pool
+    }
+}
+
+module.exports = TransactionMiner
